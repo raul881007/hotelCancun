b8a6c1c9e52f49ab385185bc8e0ab763520c4a01
raul881007 <raul881007@gmail.com>
2021-05-10 13:51:57 -0400

    Adding documentation classes

---
+++ src/test/java/com/alten/main/rest/ReservationRestTest.java
@@ -0,0 +1,19 @@
+package com.alten.main.rest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationRestTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+}
